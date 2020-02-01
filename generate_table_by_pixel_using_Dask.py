d463b39a409fb6fca7f333cd6b815b97ef16fca1
gabrielagustin <gabiagus@gmail.com>
2020-02-01 19:42:31 -0300

    add some changes

--- generate_table_by_pixel_using_Dask.py
+++ generate_table_by_pixel_using_Dask.py
@@ -13,6 +13,7 @@ Copyright (c) 2020 Your Company
 import dask.dataframe as daskDataFrame
 import numpy as np
 import pandas as pd
+from time import time
 
 import functions
 
@@ -25,6 +26,7 @@ fechaNDVI = []
 fechaMYD = []
 
 
+#
 fechaSMAP.append("2015-04-19")
 fechaSentinel.append("2015-04-18")
 fechaNDVI.append("2015-04-23")
@@ -35,6 +37,152 @@ fechaSMAP.append("2015-05-02")
 fechaSentinel.append("2015-05-02")
 fechaNDVI.append("2015-05-09")
 fechaMYD.append("2015-05-02")
+
+
+fechaSMAP.append("2015-05-10")
+fechaSentinel.append("2015-05-12")
+fechaNDVI.append("2015-05-09")
+fechaMYD.append("2015-05-12")
+
+
+fechaSMAP.append("2015-05-26")
+fechaSentinel.append("2015-05-26")
+fechaNDVI.append("2015-05-25")
+fechaMYD.append("2015-05-25")
+
+### quite porque SAR estaba cortada
+#fechaSMAP.append("2015-06-03")
+#fechaSentinel.append("2015-06-05")
+#fechaNDVI.append("2015-06-10")
+#fechaMYD.append("2015-06-05")
+
+fechaSMAP.append("2015-06-19")
+fechaSentinel.append("2015-06-19")
+fechaNDVI.append("2015-06-26")
+fechaMYD.append("2015-06-18")
+
+
+fechaSMAP.append("2015-06-30")
+fechaSentinel.append("2015-06-29")
+fechaNDVI.append("2015-06-26")
+fechaMYD.append("2015-07-03")
+
+
+fechaSMAP.append("2015-07-24")
+fechaSentinel.append("2015-07-23")
+fechaNDVI.append("2015-07-28")
+fechaMYD.append("2015-07-20")
+
+fechaSMAP.append("2015-08-17")
+fechaSentinel.append("2015-08-16")
+fechaNDVI.append("2015-08-13")
+fechaMYD.append("2015-08-13")
+#
+fechaSMAP.append("2015-08-30")
+fechaSentinel.append("2015-08-30")
+fechaNDVI.append("2015-08-29")
+fechaMYD.append("2015-08-29")
+
+fechaSMAP.append("2015-09-10")
+fechaSentinel.append("2015-09-09")
+fechaNDVI.append("2015-09-14")
+fechaMYD.append("2015-09-06")
+
+fechaSMAP.append("2015-09-23")
+fechaSentinel.append("2015-09-23")
+fechaNDVI.append("2015-09-30")
+fechaMYD.append("2015-09-22")
+#
+#
+fechaSMAP.append("2015-10-04")
+fechaSentinel.append("2015-10-03")
+fechaNDVI.append("2015-06-26")
+fechaMYD.append("2015-10-07")
+
+
+fechaSMAP.append("2015-10-28")
+fechaSentinel.append("2015-10-27")
+fechaNDVI.append("2015-10-16")
+fechaMYD.append("2015-10-24")
+
+fechaSMAP.append("2015-11-13")
+fechaSentinel.append("2015-11-10")
+fechaNDVI.append("2015-11-17")
+fechaMYD.append("2015-11-09")
+#
+#
+fechaSMAP.append("2015-11-21")
+fechaSentinel.append("2015-11-20")
+fechaNDVI.append("2015-11-17")
+fechaMYD.append("2015-11-17")
+
+fechaSMAP.append("2015-12-18")
+fechaSentinel.append("2015-12-14")
+fechaNDVI.append("2015-12-19")
+fechaMYD.append("2015-12-11")
+#
+fechaSMAP.append("2015-12-28")
+fechaSentinel.append("2015-12-28")
+fechaNDVI.append("2016-01-01")
+fechaMYD.append("2015-12-27")
+
+fechaSMAP.append("2016-01-08")
+fechaSentinel.append("2016-01-07")
+fechaNDVI.append("2016-01-01")
+fechaMYD.append("2016-01-09")
+
+fechaSMAP.append("2016-01-19")
+fechaSentinel.append("2016-01-21")
+fechaNDVI.append("2016-01-17")
+fechaMYD.append("2016-01-17")
+#
+#### quite porque SAR estaba cortada
+##fechaSMAP.append("2016-01-27")
+##fechaSentinel.append("2016-01-31")
+##fechaNDVI.append("2016-02-02")
+##fechaMYD.append("2016-01-25")
+##
+fechaSMAP.append("2016-02-14")
+fechaSentinel.append("2016-02-14")
+fechaNDVI.append("2016-02-18")
+fechaMYD.append("2016-02-10")
+#
+#### FALTA MODIS
+##fechaSMAP.append("2016-02-25")
+##fechaSentinel.append("2016-02-24")
+##fechaNDVI.append("2016-02-18")
+##fechaMYD.append("2016-02-26")
+##
+fechaSMAP.append("2016-03-12")
+fechaSentinel.append("2016-03-09")
+fechaNDVI.append("2016-03-21")
+fechaMYD.append("2016-03-13")
+
+fechaSMAP.append("2016-03-20")
+fechaSentinel.append("2016-03-19")
+fechaNDVI.append("2016-03-21")
+fechaMYD.append("2016-03-21")
+
+fechaSMAP.append("2016-04-02")
+fechaSentinel.append("2016-04-02")
+fechaNDVI.append("2016-04-06")
+fechaMYD.append("2016-04-06")
+#
+## quite por LST con nubes
+##fechaSMAP.append("2016-04-13")
+##fechaSentinel.append("2016-04-12")
+##fechaNDVI.append("2016-04-22")
+##fechaMYD.append("2016-04-22")
+#
+fechaSMAP.append("2016-04-24")
+fechaSentinel.append("2016-04-26")
+fechaNDVI.append("2016-04-22")
+fechaMYD.append("2016-04-22")
+#
+fechaSMAP.append("2016-05-20")
+fechaSentinel.append("2016-05-20")
+fechaNDVI.append("2016-05-08")
+fechaMYD.append("2016-05-20")
 
 
 
@@ -200,11 +348,23 @@ df = pd.DataFrame({'Date':_date,
         },
         columns=['Date','SM_SMAP','T_s','Sigma0','NDVI','Et','T_s_modis'])   
 
+start_time = time()
 
+df.to_csv(pathOut + "tabla_completa_Test_9km_2.csv", decimal = ",")
+print("Archivo Validacion creado con exito!")
+
+elapsed_time = time() - start_time
+print("Elapsed time for pandas: %.10f seconds." % elapsed_time)
+
+### create a dask dataframe from pandas dataframe
 daskDataFrame = daskDataFrame.from_pandas(df, npartitions=6)
-
 
 print(daskDataFrame.head())
 
+start_time = time()
+
 daskDataFrame.to_csv(pathOut + "tabla_completa_Test_9km.csv", single_file = True)
 print("Archivo Validacion creado con exito!")
+
+elapsed_time = time() - start_time
+print("Elapsed time for Dask: %.10f seconds." % elapsed_time)
