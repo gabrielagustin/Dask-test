e400fca89d26a86d91431c23560494c6257d81e4
gabrielagustin <gabiagus@gmail.com>
2020-02-02 20:08:42 -0300

    add some changes

--- generate_table_by_pixel_using_Dask.py
+++ generate_table_by_pixel_using_Dask.py
@@ -55,13 +55,27 @@ for i in range (5,6):
     src_ds_DEW, bandDEW, GeoTDEW, ProjectDEW = functions.openFileHDF(fileDEW, 1)
     # src_ds_s1, bands1, GeoTs1, Projects1 = functions.openFileHDF(fileS1, 1)
     
+    
+    ### unified resolution and sizes
+    nRow, nCol = bandNDVI.shape
+    
+    data_src = src_ds_LST
+    data_match = src_ds_NDVI
+    match = functions.matchData(data_src, data_match, type, nRow, nCol)
+    band_matchLST = match.ReadAsArray()
+    
+    data_src = src_ds_DEW
+    data_match = src_ds_NDVI
+    match = functions.matchData(data_src, data_match, type, nRow, nCol)
+    band_matchDEW = match.ReadAsArray()
+
 
 
     NDVI.append(bandNDVI.flatten())
-    LST.append(bandLST.flatten())
+    LST.append(band_matchLST.flatten())
     SWIR1.append(bandSWIR1.flatten())
     SWIR2.append(bandSWIR2.flatten())
-    # DEWPOINT.append(bandDEW.flatten())
+    DEWPOINT.append(band_matchDEW.flatten())
     # s1.append(bands1.flatten())
     Month.append(i)
 
@@ -69,7 +83,7 @@ _NDVI = np.array(NDVI).flatten()
 _LST = np.array(LST).flatten()
 _SWIR1 = np.array(SWIR1).flatten()
 _SWIR2 = np.array(SWIR2).flatten()
-# _DEWPOINT = np.array(DEWPOINT).flatten()
+_DEWPOINT = np.array(DEWPOINT).flatten()
 # _s1 = np.array(s1).flatten()
 
 
@@ -79,26 +93,34 @@ _SWIR2[_SWIR2>6000] = 0
 _SWIR1 = (_SWIR1-np.min(_SWIR1))/(np.max(_SWIR1)-np.min(_SWIR1))
 _SWIR2 = (_SWIR2-np.min(_SWIR2))/(np.max(_SWIR2)-np.min(_SWIR2))
 
-print(_NDVI.shape)
-print(_LST.shape)
-print(_SWIR1.shape)
-print(_SWIR2.shape)
+# print(_NDVI.shape)
+# print(_LST.shape)
+# print(_SWIR1.shape)
+# print(_SWIR2.shape)
+# print(_LST.shape)
+# print(_DEWPOINT.shape)
+
 
 df = pd.DataFrame({'NDVI':_NDVI,
         'SWIR1':_SWIR1, 
-        'SWIR2':_SWIR2},
-        columns=['NDVI', 'SWIR1', 'SWIR2'])   
+        'SWIR2':_SWIR2
+        'LST': _LST,
+        'DEWPOINT': _DEWPOINT},
+        columns=['NDVI', 'SWIR1', 'SWIR2', 'LST', 'DEWPOINT'])   
 
 df = df.replace(np.nan, -1)
 
-start_time = time()
+#### test dataframe pandas create .csv file
+# start_time = time()
 
-df.to_csv(pathOut + "tabla1.csv", decimal = ",")
-print("Archivo Validacion creado con exito!")
+# df.to_csv(pathOut + "tabla1.csv", decimal = ",")
+# print("Archivo Validacion creado con exito!")
 
-elapsed_time = time() - start_time
-print("Elapsed time for pandas: %.10f seconds." % elapsed_time)
+# elapsed_time = time() - start_time
+# print("Elapsed time for pandas: %.10f seconds." % elapsed_time)
 
+
+#### test dataframe Dask create .csv file
 ### create a dask dataframe from pandas dataframe
 daskDataFrame = daskDataFrame.from_pandas(df, npartitions=6)
 
